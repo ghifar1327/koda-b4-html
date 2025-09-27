a0c5f613def7532a0d136a151fa8df030d84274d
ghifar1327 <givartorreto@gmail.com>
2025-09-27 20:12:48 +0700

    feat: make all feature buttons work

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,36 @@ define(["jquery"], function ($) {
     });
   });
 
-    $(document).ready(()=>{
-        
+  $(document).ready(() => {
+    $("#btnTanggal").on("click", (e) => {
+      e.preventDefault();
+      $("#btn-t1").toggleClass("hidden block");
+      $("#btn-t2").toggleClass("block hidden");
+      $("#optionTanggal").toggleClass("block hidden");
+    });
+  });
+
+  $(document).ready(() => {
+    $("#btnOption").on("click", (e) => {
+      e.preventDefault();
+      $("#optionRd").toggleClass("block hidden");
+    });
+  });
+  $(document).ready(() =>{
+    $('#btn-arrow1').on('click', (e)=>{
+        e.preventDefault()
+        $('#subtask').toggleClass('block hidden')
+        $('#arrow1').toggleClass('hidden block')
+        $('#arrow2').toggleClass('block hidden')
     })
+  })
+  $(document).ready(() =>{
+    $('#btn-arrowFoot').on('click', (e)=>{
+        e.preventDefault()
+        $('#tComplete').toggleClass('block hidden')
+        $('#btnFoot1').toggleClass('hidden block')
+        $('#btnFoot2').toggleClass('block hidden')
+    })
+  })
+
 });
